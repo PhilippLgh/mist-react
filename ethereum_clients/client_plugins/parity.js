b26d8d0c2ddcac612d266a34a7e5b9dcd2e91644
Marc Garreau <marcdgarreau@gmail.com>
2019-04-17 14:38:12 -0600

    fix: parity IPC path

--- ethereum_clients/client_plugins/parity.js
+++ ethereum_clients/client_plugins/parity.js
@@ -1,3 +1,23 @@
+function findIpcPath() {
+  let ipcPath
+
+  switch (process.platform) {
+    case 'win32': {
+      ipcPath = `${process.env.APPDATA}\\Local\\Parity\\Ethereum\\jsonrpc.ipc`
+    }
+    case 'linux': {
+      ipcPath = `~/.local/share/io.parity.ethereum/jsonrpc.ipc`
+    }
+    case 'darwin': {
+      ipcPath = `~/Library/Application\ Support/io.parity.ethereum/jsonrpc.ipc`
+    }
+    default: {
+    }
+  }
+
+  return ipcPath
+}
+
 module.exports = {
   type: 'client',
   order: 2,
@@ -6,5 +26,6 @@ module.exports = {
   // repository: 'https://github.com/paritytech/parity-ethereum'
   repository: 'https://github.com/PhilippLgh/EthCapetownWorkshop',
   prefix: `${process.platform}`, // filter github assets
-  binaryName: process.platform === 'win32' ? 'parity.exe' : 'parity'
+  binaryName: process.platform === 'win32' ? 'parity.exe' : 'parity',
+  resolveIpc: () => findIpcPath()
 }
