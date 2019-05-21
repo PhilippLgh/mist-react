d5ae1bb8758b53fa34dcfefc5c532ed2a6cc66a5
Everton Fraga <ev@ethereum.org>
2019-05-21 10:20:39 -0400

    test: adding failing test for #23

--- test/e2e/acceptance.test.js
+++ test/e2e/acceptance.test.js
@@ -21,7 +21,6 @@ test.beforeEach(async t => {
 
   t.context.app = ApplicationFactory.development()
 
-  // console.log(t.context.app);
   await t.context.app.start()
 })
 
@@ -112,13 +111,54 @@ test('As a user, I want to know if my client is up to date', async t => {
   t.pass()
 })
 
+test('As a user, I want to see sync status visually', async t => {
+  t.fail('Not implemented.')
+})
+
+test('As a user, I want to have the connection details remembered', async t => {
+  const {app, client, win} = await init(t)
+  const versionList = new VersionList(app.client)
+  const clientAppBar = new ClientAppBar(app.client)
+  const settings = new ClientSettingsForm(app.client)
+
+  await versionList.waitToLoad()
+  await versionList.clickOnItem(0)
+  await versionList.waitUntilVersionSelected(0)
+
+  await clientAppBar.settings.click()
+  await settings.getPathInput('dataDir').setValue('/tmp/ac5718')
+
+  // Restart the app
+  await app.stop()
+  t.context.app = ApplicationFactory.development()
+  const app2 = t.context.app
+  await t.context.app.start()
+
+  const clientAppBar2 = new ClientAppBar(app2.client)
+  await clientAppBar2.settings.click()
+
+  const settings2 = new ClientSettingsForm(app2.client)
+  const dataDirValue = await settings2.getPathInput('dataDir').getValue()
+
+  t.is(dataDirValue, '/tmp/ac5718')
+})
+
 // OK - As a user, I want to download a geth node
 // OK - As a user, I want to start/stop my geth node from the app UI. #38
 // OK - As a user, I want to configure my node settings and options easily. #37
 // OK - As a user, I want to be notified when a new version of my node is available, so I don't fork. #22
-// As a user, I want to see sync status visually, so I don't have to parse the logs and guess #73
-// As a user, I want to have the connection details remembered, so I can have a consistent use of the app #23
-// As a user, I want to be reminded of updates on the app itself, so I can get latest features and fixes. #33
 // OK - As a user, I want to provide an existing network data directory, so that I don't have two copies of the network. #35
+
+// As a user, I want to have the connection details remembered, so I can have a consistent use of the app #23
+
+// Waiting for PR
+// As a user, I want to see sync status visually, so I don't have to parse the logs and guess #73
+
+// Should be tested
 // As a user, I want to download codesigned applications, so it works without nasty warnings on my OS #114
+
+// Spectron menu plugin needed
 // As a developer, I want to test Grid-UI build channels from the Grid [shell] interface, so we can ensure app quality standards #87
+
+// MOCK NEEDED
+// As a user, I want to be reminded of updates on the app itself, so I can get latest features and fixes. #33
