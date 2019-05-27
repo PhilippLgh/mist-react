2a60599e9fb17c86331c0aa7a1e4cc8d42de8852
philipplgh <philipplgh@gmail.com>
2019-05-27 19:30:59 +0200

    chore: cleanup

--- index.js
+++ index.js
@@ -40,10 +40,6 @@ const CONFIG_NAME = '.shell.config.js'
 // hw acceleration can cause problem in VMs and in certain APIs
 app.disableHardwareAcceleration()
 
-const WindowManager = require('./WindowManager')
-// TODO move into WindowManager
-let mainWindow = null
-
 const shellManager = new AppManager({
   repository: 'https://github.com/ethereum/grid',
   auto: true,
