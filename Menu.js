f1cfacc556f1d3e78f7cb3e613f0bb2b79248eb3
Ryan Ghods <ryan@ryanio.com>
2019-06-26 17:44:39 -0700

    chore: adds copy-paste functionality for darwin

--- Menu.js
+++ Menu.js
@@ -5,7 +5,17 @@ module.exports.getMenuTemplate = () => {
   const template = [
     {
       label: 'Edit',
-      submenu: [{ role: 'undo' }, { role: 'redo' }]
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' }
+      ]
     },
     {
       label: 'View',
