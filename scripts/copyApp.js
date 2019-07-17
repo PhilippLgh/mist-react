c67da6459b13700de73318c9af09f5ad937523c6
philipplgh <philipplgh@gmail.com>
2019-07-17 18:03:33 +0200

    update copy script

--- scripts/copyApp.js
+++ scripts/copyApp.js
@@ -1,6 +1,6 @@
-// TODO executed before electron builder builds: copies or downloads with app-updater a version of the latest .asar to this dir for shipping
-// TODO make sure there is not more than one asar present
-
+/**
+ *
+ */
 const path = require('path')
 const { AppManager } = require('@philipplgh/electron-app-manager')
 
@@ -9,6 +9,9 @@ const updater = new AppManager({
 })
 
 ;(async function() {
-  const latest = await updater.getLatest()
-  await updater.download(latest, true, path.join(__dirname, '..'))
+  const latest = await updater.getLatestRemote()
+  await updater.download(latest, {
+    targetDir: path.join(__dirname, '..', 'cached-grid-ui'),
+    writeDetachedMetadata: false
+  })
 })()
