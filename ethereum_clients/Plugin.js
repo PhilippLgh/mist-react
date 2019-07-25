6256ed1ce4b100f8f41dac38e5fe8cd8e460652b
Ryan Ghods <ryan@ryanio.com>
2019-07-24 19:43:21 -0600

    chore: fix relaying pluginError

--- ethereum_clients/Plugin.js
+++ ethereum_clients/Plugin.js
@@ -80,7 +80,7 @@ class Plugin extends EventEmitter {
   registerEventListeners(sourceEmitter, destEmitter) {
     // FIXME memory leaks start here:
     // forward all events from the spawned process
-    let eventTypes = ['newState', 'error', 'log', 'notification']
+    let eventTypes = ['newState', 'error', 'log', 'notification', 'pluginError']
     eventTypes.forEach(eventName => {
       sourceEmitter.on(eventName, arg => {
         if (eventName !== 'log') {
